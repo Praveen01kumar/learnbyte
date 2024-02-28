cebe883115692a7b14ee776e81ec6fc909d0373d
Praveen01kumar <pk2414089@gmail.com>
2024-02-28 15:25:08 +0530

    developer console and codeediter post added

--- src/app/courses/javascript/lesssion/code-editors/code-editors.component.ts
+++ src/app/courses/javascript/lesssion/code-editors/code-editors.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RsideBar } from 'src/app/shared/interfaces/interfaces';
+import { SharedService } from 'src/app/shared/service/shared-service';
 
 @Component({
   selector: 'app-code-editors',
   templateUrl: './code-editors.component.html',
   styleUrls: ['./code-editors.component.scss']
 })
-export class CodeEditorsComponent {
+export class CodeEditorsComponent implements OnInit, OnDestroy {
+
+  constructor(public shearedService: SharedService) { }
+
+  ngOnInit(): void { this.callOninIt(); }
+
+  callOninIt() {
+    const data: RsideBar[] = [
+      { name: "Understanding Code Editors", id: "Understanding_Code_Editors" },
+      { name: "Syntax Highlighting in Visual Studio Code", id: "Syntax_Highlighting_in_Visual_Studio_Code" },
+      { name: "Conclusion", id: "code_editors_conclusion" },
+
+    ];
+    this.shearedService.rSidebar_Id_Data$.next(data);
+  }
+
+  code: string = `
+  // JavaScript code snippet with syntax highlighting
+  function greet(name) {
+    return "Hello, " + name + "!";
+  }
+  
+  console.log(greet("World"));
+  `;
+
+  ngOnDestroy(): void { this.shearedService.rSidebar_Id_Data$.next([]); }
 
 }
--- src/app/courses/javascript/lesssion/developer-console/developer-console.component.ts
+++ src/app/courses/javascript/lesssion/developer-console/developer-console.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RsideBar } from 'src/app/shared/interfaces/interfaces';
+import { SharedService } from 'src/app/shared/service/shared-service';
 
 @Component({
   selector: 'app-developer-console',
   templateUrl: './developer-console.component.html',
   styleUrls: ['./developer-console.component.scss']
 })
-export class DeveloperConsoleComponent {
+export class DeveloperConsoleComponent implements OnInit, OnDestroy {
 
+  constructor(public shearedService: SharedService) { }
+
+  ngOnInit(): void { this.callOninIt(); }
+
+  callOninIt() {
+    const data: RsideBar[] = [
+      { name: "Overview of Developer Console", id: "Overview_of_Developer_Console" },
+      { name: "Inspecting and Modifying Elements", id: "Inspecting_and_Modifying_Elements" },
+      { name: "Debugging JavaScript", id: "Debugging_JavaScript" },
+      { name: "Conclusion", id: "Developer_Console_Conclusion" },
+
+    ];
+    this.shearedService.rSidebar_Id_Data$.next(data);
+  }
+
+  code: string = `
+// Select an element by its ID
+var element = document.getElementById('example-element');
+
+// Modify its text content
+element.textContent = 'New Content';
+
+// Change its background color
+element.style.backgroundColor = 'lightblue';
+`;
+  code1: string = `
+function calculateSum(a, b) {
+    var sum = a + b;
+    debugger; // Set a breakpoint
+    return sum;
+}
+
+var result = calculateSum(5, 3);
+console.log(result);
+`;
+
+  ngOnDestroy(): void { this.shearedService.rSidebar_Id_Data$.next([]); }
 }
